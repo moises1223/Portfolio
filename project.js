703c2ed27323e959a09bc3fdcc3a2a4695bf0ddd
moises1223 <daorahum@gmail.com>
2023-03-01 21:47:15 -0300

    navbar made

---
+++ project.js
@@ -0,0 +1,11 @@
+let projectData =[
+    {
+        image: 'img/project-1.png',
+        name: 'project one',
+        detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, augue quis rutrum auctor, erat est mattis velit, vel luctus est nisl',
+        github: '#',
+        live: '#',
+        tags: '#javascript, #fullstack, #css'
+    },
+    
+]
